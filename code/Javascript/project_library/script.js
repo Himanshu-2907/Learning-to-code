09ab7d5359928927bd4c767ec630826ed87b4459
Himanshu-2907 <pradhanhimanshu86929@gmail.com>
2026-04-20 14:48:29 +0530

    Render books in library container
    Populate the #library container with each book's title, author, pages and read status by setting div.innerHTML and appending the div for each book. Add explanatory comments for the container and call displaybooks() at the end to render the list on load.

--- code/Javascript/project_library/script.js
+++ code/Javascript/project_library/script.js
@@ -15,9 +15,33 @@ console.log(mylibrary)
 
 function displaybooks(){
     const container=document.getElementById("library")
-    container.innerHTML=""
+    container.innerHTML=""  //this line removes everything inside the container
+    //container is a variable that holds a DOM element
+    // a container contains all the books elements
     mylibrary.forEach(book =>{
-        const div=document.createElement("div")
+        const div=document.createElement("div") // and empty HTML element is created in memory
         
+        div.innerHTML=`
+        <h3>${book.title}</h3>
+        <p>${book.author}</p>
+        <p>${book.pages} pages</p>
+        <p>${book.read ? "Read": "Not Read"}</p>
+        `
+        container.appendChild(div)
     })
 }
+
+displaybooks()
+
+document.getElementById("bookform").addEventListener("submit",function(e)
+// document is built in object that represents entire html webpage
+//event listener is used to add a user action and wait for the action to take place
+//submit is trigger and then the callback function runs
+{
+    const title=document.getElementById("title").value;
+    const author=document.getElementById("author").value;
+    const pages=document.getElementById("pages").value;
+    const read=document.getElementById("read").checked;
+    addBookToLibrary(title,author,pages,read);
+    displaybooks();
+})
