8647ba300e879866c2a68504441ba01387495d09
Himanshu-2907 <pradhanhimanshu86929@gmail.com>
2026-04-20 13:28:25 +0530

    Update script.js

--- code/Javascript/project_library/script.js
+++ code/Javascript/project_library/script.js
@@ -12,3 +12,12 @@ function addBookToLibrary(title,author,pages,read){
 }
 addBookToLibrary("One Piece","Oda",1000,true)
 console.log(mylibrary)
+
+function displaybooks(){
+    const container=document.getElementById("library")
+    container.innerHTML=""
+    mylibrary.forEach(book =>{
+        const div=document.createElement("div")
+        
+    })
+}
