d2050dc8170b779fba606f45ead1b4c84132c23e
Himanshu-2907 <pradhanhimanshu86929@gmail.com>
2026-04-21 17:09:40 +0530

    Add webpack build output & update config
    Add generated webpack build artifacts (dist/index.html, dist/main.js) and update project configuration for webpack. Modified webpack.config.js and package.json (and regenerated package-lock.json). Add styling and assets (src/styles.css, src/my-image.png), update template.html, and adjust src/index.js to import styles and log the new greeting module.

--- code/Javascript/webpack-practice/src/index.js
+++ code/Javascript/webpack-practice/src/index.js
@@ -1,3 +1,7 @@
+import "./styles.css";  // ADD THIS LINE
 import { greeting } from "./greeting.js";
-
+import myImage from "./my-image.png"
 console.log(greeting);
+const img=document.createElement("img")
+img.src=myImage
+document.body.appendChild(img)
--- code/Javascript/webpack-practice/webpack.config.js
+++ code/Javascript/webpack-practice/webpack.config.js
@@ -1,4 +1,5 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   mode: "development",
@@ -7,5 +8,26 @@ export default {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer:{
+    watchFiles:['./src/template.html']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {  //this is the loader defines how to process diff file types like the png file 
+    rules: [
+      {
+        test: /\.css$/i,  
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test:/\.(png|svg|jpg|jpeg)$/i,
+        type:"asset/resource"
+      },
+    ],
   },
 };
