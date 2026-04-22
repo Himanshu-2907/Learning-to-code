4da9843efce5aee87d158bb897aba6779b848830
Himanshu-2907 <pradhanhimanshu86929@gmail.com>
2026-04-22 12:59:25 +0530

    Update webpack.config.js

--- code/Javascript/restaurant page/webpack.config.js
+++ code/Javascript/restaurant page/webpack.config.js
@@ -1,5 +1,28 @@
-import pat from "node:path"
-import HtmlWebpackPluginfrom from "html-webpack-plugin"
+import path from "node:path"
+import HtmlWebpackPlugin from "html-webpack-plugin"
 export default{
-    
+    mode:"development",
+    entry: "./src/index.js",
+    output:{
+        filename:'main.js',
+        path:path.resolve(import.meta.dirname,'dist'),
+        clean:true
+    },
+    devtool: 'eval-source-map',
+    devServer:{
+        watchFiles:['./src/template.html'],
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/template.html',
+        }),
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader']
+            }
+        ]
+    }
 }
