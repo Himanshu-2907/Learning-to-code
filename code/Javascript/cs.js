9881fae397ad677b720a51109a72d5b2f5ebe8f8
Himanshu-2907 <pradhanhimanshu86929@gmail.com>
2026-04-23 16:50:00 +0530

    Add React + Vite scaffold and JS examples
    Add a new React + Vite app scaffold (code/react/my-react-app) including package.json, package-lock.json, .gitignore, vite and ESLint configs, index.html, public assets, and basic src files (App, Header, Footer, Food, main.jsx, index.css). Also add a small JavaScript example (code/Javascript/cs.js) containing simple Fibonacci utilities and a recursive counter. This is an initial project setup to start development with HMR and linting.

---
+++ code/Javascript/cs.js
@@ -0,0 +1,24 @@
+//fibonacci
+function lonumber(num,i=1){
+    if(i<=num)
+    {
+        console.log(i)
+        lonumber(num,i+1)
+    }
+}
+lonumber(10)
+
+function fibs(n)
+{
+    if(n<=0) return []
+    if(n===2)return []
+    const result=[0,1]
+    for(let i=2;i<n;i++)
+    {
+        result.push(result[i-1]+result[i-2])
+    }
+    return result
+}
+console.log(fibs(8))
+console.log(fibs(1))
+console.log(fibs(0))
---
+++ code/react/my-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){  // a container is like a section of resuable jsx code, it's like html
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} Your Website</p>
+        </footer>
+    )
+}
+export default Footer;
