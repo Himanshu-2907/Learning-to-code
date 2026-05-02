c893ef6cea139fc10857dd8437945fab8bf8607b
Himanshu-2907 <pradhanhimanshu86929@gmail.com>
2026-05-02 10:23:18 +0530

    Add TypeScript practice files and config
    Add a simple practice TypeScript module (fibonacci.ts) and its compiled artifacts (fibonacci.js, .d.ts and source maps). Add project configuration (tsconfig.json in both code/ and code/Javascript/practice/) and package metadata (package.json and package-lock.json). Note: package.json declares TypeScript and also contains a misspelled 'typecript' entry.

---
+++ code/Javascript/practice/fibonacci.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var x = 12;
+function abcd() {
+    console.log('hii');
+}
+//# sourceMappingURL=fibonacci.js.map
---
+++ code/Javascript/practice/fibonacci.ts
@@ -0,0 +1,4 @@
+var x=12;
+function abcd(){
+    console.log('hii')
+}
